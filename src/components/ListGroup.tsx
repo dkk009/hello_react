326795703610add8f653eddc9ce82e5acdac2237
dkk009 <dkk0011@gmail.com>
2023-07-06 17:29:30 +0530

    Handled list item click

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,29 @@
 import { Fragment } from "react";
 
 function ListGroup() {
-  const cityNames = ["Delhi", "Banglore", "Chennai", "Mumbai", "Jaipur"];
+  let cityNames = ["Delhi", "Banglore", "Chennai", "Mumbai", "Jaipur"];
 
+  const getMessage = () => {
+    return cityNames.length === 0 ? <p>No item found</p> : null;
+  };
+  const handEvent = (event: MouseEvent) => {
+    console.log("You clicked me");
+  };
   return (
     <>
       <h1>City Names</h1>
+      {cityNames.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {cityNames.map((city) => (
-          <li key={city}> {city}</li>
+        {cityNames.map((city, index) => (
+          <li
+            className="list-group-item"
+            key={city}
+            onClick={(event) =>
+              console.log("You clicked me:", city, index, " Pos", event)
+            }
+          >
+            {city}
+          </li>
         ))}
       </ul>
     </>
